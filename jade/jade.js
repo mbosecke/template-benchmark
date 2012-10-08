b44bf379bc576151bbeb57c165c64a0d87641e39
Vadim M. Bryshev <vadimbaryshev@gmail.com>
2012-10-08 14:10:55 +0400

    Fix Jade test

--- jade/jade.js
+++ jade/jade.js
@@ -6,14 +6,14 @@ var tplData;
 module.exports.prepare = function (data, done) {
 	var str = fs.readFileSync(__dirname + '/tpl_escaped.jade', 'utf8');
 	tplData = data;
-	compiled = jade.compile(str, data);
+	compiled = jade.compile(str);
 	done();
 };
 
 module.exports.prepareUnescaped = function (data, done) {
 	var str = fs.readFileSync(__dirname + '/tpl_unescaped.jade', 'utf8');
 	tplData = data;
-	compiled = jade.compile(str, data);
+	compiled = jade.compile(str);
 	done();
 };
 
