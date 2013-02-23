beb66d34fd1e7e330955003557668756de065e9e
Roel van Uden <roel.van.uden@hotmail.com>
2013-02-23 20:02:39 +0100

    Gaikan 1.3.3. Corrected terrible mistake in HTML.
    Wondered about the odd performance difference between Gaikan and other
    engines. Turns out, the template was incorrect. Previous results are
    unreliable, this patches it up and brings performance in line with
    expectations.

--- gaikan/gaikan.js
+++ gaikan/gaikan.js
@@ -3,21 +3,19 @@ var runtime = require('gaikan/lib/runtime');
 var compiled;
 var tplData;
 
-gaikan.options.directory = 'gaikan';
-
 module.exports.prepare = function (data, done) {
 	tplData = data;
-	compiled = gaikan.compileFromPath('tpl_escaped.html');
+	compiled = gaikan.compileFile('tpl_escaped', 'gaikan', undefined, true);
 	done();
 };
 
 module.exports.prepareUnescaped = function (data, done) {
 	tplData = data;
-	compiled = gaikan.compileFromPath('tpl_unescaped.html');
+	compiled = gaikan.compileFile('tpl_unescaped', 'gaikan', undefined, true);
 	done();
 };
 
 module.exports.step = function (done) {
-	var html = compiled(runtime, tplData);
+	var html = compiled(tplData);
 	done(undefined, html);
 };
