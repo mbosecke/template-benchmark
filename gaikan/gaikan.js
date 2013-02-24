96c347e78b29f998c3fcdb88189b6c9108a0602f
Roel van Uden <roel.van.uden@hotmail.com>
2013-02-24 13:00:14 +0100

    Gaikan 1.3.4. Use optimized data-for iterator.
    Gaikan notes:
    Implemented faster scoping mechanism for iterations.
    Intermediate instructions for post-parser optimizations.
    Introduced data-for statement for array-iteration.

--- gaikan/gaikan.js
+++ gaikan/gaikan.js
@@ -1,17 +1,22 @@
 var gaikan = require('gaikan');
-var runtime = require('gaikan/lib/runtime');
 var compiled;
 var tplData;
 
 module.exports.prepare = function (data, done) {
 	tplData = data;
 	compiled = gaikan.compileFile('tpl_escaped', 'gaikan', undefined, true);
+	/*gaikan.explainFile('tpl_escaped', 'gaikan', function (js) {
+		console.log(js.replace(/\t/g, '  '));
+	});*/
 	done();
 };
 
 module.exports.prepareUnescaped = function (data, done) {
 	tplData = data;
 	compiled = gaikan.compileFile('tpl_unescaped', 'gaikan', undefined, true);
+	/*gaikan.explainFile('tpl_unescaped', 'gaikan', function (js) {
+		console.log(js.replace(/\t/g, '  '));
+	});*/
 	done();
 };
 
