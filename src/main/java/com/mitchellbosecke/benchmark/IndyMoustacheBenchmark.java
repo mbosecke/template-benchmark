e5fca7f90070890c20ae079925764bf5dbe18185
Sam Pullara <spullara@yahoo.com>
2015-02-27 13:18:57 -0800

    turn off HTML encoding as per the specification for the benchmark

--- src/main/java/com/mitchellbosecke/benchmark/IndyMoustacheBenchmark.java
+++ src/main/java/com/mitchellbosecke/benchmark/IndyMoustacheBenchmark.java
@@ -2,11 +2,13 @@ package com.mitchellbosecke.benchmark;
 
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheException;
 import com.github.mustachejava.MustacheFactory;
 import com.github.mustachejava.indy.IndyObjectHandler;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Setup;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Map;
@@ -19,7 +21,17 @@ public class IndyMoustacheBenchmark extends BaseBenchmark {
 
     @Setup
     public void setup() {
-        DefaultMustacheFactory mustacheFactory = new DefaultMustacheFactory();
+        DefaultMustacheFactory mustacheFactory = new DefaultMustacheFactory() {
+          @Override
+          public void encode(String value, Writer writer) {
+            // Disable HTML escaping
+            try {
+              writer.write(value);
+            } catch (IOException e) {
+              throw new MustacheException(e);
+            }
+          }
+        };
         mustacheFactory.setObjectHandler(new IndyObjectHandler());
         template = mustacheFactory.compile("templates/stocks.mustache.html");
         this.context = getContext();
--- src/main/java/com/mitchellbosecke/benchmark/MoustacheBenchmark.java
+++ src/main/java/com/mitchellbosecke/benchmark/MoustacheBenchmark.java
@@ -1,9 +1,11 @@
 package com.mitchellbosecke.benchmark;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Map;
 
+import com.github.mustachejava.MustacheException;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Setup;
 
@@ -19,7 +21,17 @@ public class MoustacheBenchmark extends BaseBenchmark {
 
     @Setup
     public void setup() {
-        MustacheFactory mustacheFactory = new DefaultMustacheFactory();
+        MustacheFactory mustacheFactory = new DefaultMustacheFactory() {
+          @Override
+          public void encode(String value, Writer writer) {
+            // Disable HTML escaping
+            try {
+              writer.write(value);
+            } catch (IOException e) {
+              throw new MustacheException(e);
+            }
+          }
+        };
         template = mustacheFactory.compile("templates/stocks.mustache.html");
         this.context = getContext();
     }
