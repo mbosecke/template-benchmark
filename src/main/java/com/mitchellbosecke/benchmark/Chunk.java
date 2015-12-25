65c2310551a07f7b290bdf7b8115fc003cffd728
Václav Kužel <vkuzel@gmail.com>
2015-12-25 13:27:41 +0100

    Fixed getting templates path for Chunk templates test.

--- src/main/java/com/mitchellbosecke/benchmark/Chunk.java
+++ src/main/java/com/mitchellbosecke/benchmark/Chunk.java
@@ -4,20 +4,30 @@ import com.x5.template.Theme;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Setup;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
 
 public class Chunk extends BaseBenchmark {
 
     private com.x5.template.Chunk chunk;
 
-    @Setup
-    public void setup() throws URISyntaxException {
-        URI templatesUri = this.getClass().getResource("/templates").toURI();
-        File templatesDir = new File(templatesUri);
+    private String getResourcePath(String location) throws IOException {
+        URL resourceUrl = this.getClass().getResource(location);
+        switch (resourceUrl.getProtocol()) {
+            case "file":
+                return resourceUrl.getFile();
+            case "jar":
+                JarURLConnection jarUrl = (JarURLConnection) resourceUrl.openConnection();
+                return jarUrl.getJarFile().getName();
+            default:
+                throw new IllegalArgumentException("Invalid location!");
+        }
+    }
 
-        Theme theme = new Theme(templatesDir.toString(), "");
+    @Setup
+    public void setup() throws IOException {
+        Theme theme = new Theme(getResourcePath("/templates"), "");
         chunk = theme.makeChunk("stocks.chunk");
     }
 
